c8eca2db8574143466e92beeab01e46fa6828840
Anufether <anufether@outlook.com>
2024-08-07 11:30:41 +0800

    feat: 项目初始化

---
+++ src/main/java/top/anufether/pixiv/Main.java
@@ -0,0 +1,16 @@
+package top.anufether.pixiv;
+
+/**
+ * @Project: pixiv-reptile
+ * @Package: top.anufether.pixiv
+ * @Author: anufether
+ * @Create: 2024/8/7 11:10
+ * @Description: this file is used for ...
+ * @History: modify
+ * * 1900-01-01 12:00:00 modified by xxx
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
